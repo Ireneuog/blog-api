f0fb5b745aff3c4686565ed484c6f863010ffd3c
João <jplp2004@gmail.com>
2025-11-20 18:54:18 +0000

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,88 +1,24 @@
 import express from "express";
-import { prisma } from "./prisma/client";
 import commentRoutes from "./routes/commentRoutes";
+import postRoutes from "./routes/postRoutes";
 import { requestLogger } from "./middlewares/requestLogger";
 
 const app = express();
-const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
-// Observabilidade 
+// Monitorizaç
 app.use(requestLogger);
 
-// Health check (Sprint 3)
+// Health check 
 app.get("/health", (req, res) => {
   res.json({ status: "ok" });
 });
 
-// POST /comments/:postId → cria comentário
-// GET  /comments/:postId → lista comentários
+// Rotas de comentários
 app.use("/comments", commentRoutes);
 
-app.put("/posts/:id", async (req, res) => {
-  try {
-    const postId = Number(req.params.id);
-    const { title, content } = req.body;
-
-    const userId = 1;
-
-    const post = await prisma.post.findUnique({ where: { id: postId } });
-    if (!post) {
-      return res.status(404).json({ error: "Post não encontrado" });
-    }
-
-    if (post.userId !== userId) {
-      return res
-        .status(403)
-        .json({ error: "Não tens permissão para editar este post" });
-    }
-
-    const updated = await prisma.post.update({
-      where: { id: postId },
-      data: { title, content },
-    });
-
-    res.json(updated);
-  } catch (err: any) {
-    res.status(500).json({ error: err.message || "Erro ao editar post" });
-  }
-});
-
-app.delete("/posts/:id", async (req, res) => {
-  try {
-    const postId = Number(req.params.id);
-
-    const userId = 1;
-
-    const post = await prisma.post.findUnique({ where: { id: postId } });
-    if (!post) {
-      return res.status(404).json({ error: "Post não encontrado" });
-    }
-
-    if (post.userId !== userId) {
-      return res
-        .status(403)
-        .json({ error: "Não tens permissão para apagar este post" });
-    }
-
-    // Apagar comentários ligados ao post
-    await prisma.comment.deleteMany({
-      where: { postId },
-    });
-
-    await prisma.post.delete({
-      where: { id: postId },
-    });
-
-    res.status(204).send();
-  } catch (err: any) {
-    res.status(500).json({ error: err.message || "Erro ao apagar post" });
-  }
-});
-
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+// Rotas de posts (editar/apagar)
+app.use("/posts", postRoutes);
 
 export default app;
